a2c93c9b0103f5f3f0620725af10fb231c0fa73b
Vale <valentina.ligueno@gmail.com>
2019-10-09 12:23:43 -0300

    modificación de package

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    runtimeCompiler: true
-  };
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+          options.prettify = false
+          return options
+        })
+  }
+}
