f727386dd8497de0155ee9a3fe3a34a08a90c7e5
Vale <valentina.ligueno@gmail.com>
2019-10-09 14:12:28 -0300

    back in time

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
+//import SchedulePlugin from '@syncfusion/ej2-vue-schedule';
 
 /*import { loadCldr} from '@syncfusion/ej2-base';
 
