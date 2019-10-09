92a6b90c9095a22d1afc4a9b1d38dc508ad9fbba
Vale <valentina.ligueno@gmail.com>
2019-10-09 12:34:38 -0300

    runtime compiler

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-        .loader('vue-loader')
-        .tap(options => {
-          options.prettify = false
-          return options
-        })
-  }
-}
+    runtimeCompiler: true
+  };
